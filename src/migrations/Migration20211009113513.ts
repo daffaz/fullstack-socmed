dc786aa4bb7f387962f5809589a1b37469446860
Daffa <67418594+daffaz@users.noreply.github.com>
2021-10-12 19:19:25 +0700

    refactor

--- src/migrations/Migration20211009113513.ts
+++
@@ -1,9 +0,0 @@
-import { Migration } from '@mikro-orm/migrations';
-
-export class Migration20211009113513 extends Migration {
-
-  async up(): Promise<void> {
-    this.addSql('create table `post` (`id` int unsigned not null auto_increment primary key, `title` text not null, `created_at` datetime not null, `updated_at` datetime not null) default character set utf8mb4 engine = InnoDB;');
-  }
-
-}
