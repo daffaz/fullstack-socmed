8267a1efc8ee51c72bb606e336ef479d2e8bf115
Daffa <67418594+daffaz@users.noreply.github.com>
2021-10-12 23:30:12 +0700

    register page

--- web/src/components/Wrapper.tsx
+++ web/src/components/Wrapper.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@chakra-ui/react";
+import { Box, useColorMode } from "@chakra-ui/react";
 import React from "react";
 
 interface WrapperProps {
@@ -6,9 +6,16 @@ interface WrapperProps {
 }
 
 const Wrapper: React.FC<WrapperProps> = ({ children, variant = "regular" }) => {
+  const { colorMode } = useColorMode();
+  const bgColor = { light: "gray.50", dark: "gray.900" };
+  const color = { light: "black", dark: "white" };
+
   return (
     <Box
+      h="100vh"
       w="100%"
+      bg={bgColor[colorMode]}
+      color={color[colorMode]}
       display="flex"
       flexDir="column"
       alignItems="center"
--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -1,36 +1,56 @@
-import { Box, FormControl, FormLabel, Input } from "@chakra-ui/react";
-import { Field, Form, Formik } from "formik";
+import { Box, Button } from "@chakra-ui/react";
+import { Form, Formik } from "formik";
 import React from "react";
+import { useMutation } from "urql";
+import { DarkModeSwitch } from "../components/DarkModeSwitch";
+import InputField from "../components/InputField";
 import Wrapper from "../components/Wrapper";
 
 interface registerProps {}
 
 const Register: React.FC<registerProps> = ({}) => {
+  const REGISTER_MUTATION = `
+  
+  `;
+  const [, register] = useMutation(REGISTER_MUTATION);
   return (
     <Wrapper variant="small">
       <Box maxW="800px" w="100%" pt={8}>
+        <DarkModeSwitch />
         <Formik
           initialValues={{ username: "", password: "" }}
-          onSubmit={(values) => {
+          onSubmit={async (values) => {
             console.log(values);
+            const response = await register({
+              username: values.username,
+              password: values.password,
+            });
           }}
         >
-          {({ values, handleChange }) => (
+          {({ isSubmitting }) => (
             <Form>
-              <Field>
-                {() => (
-                  <FormControl>
-                    <FormLabel htmlFor="username">Username</FormLabel>
-                    <Input
-                      value={values.username}
-                      onChange={handleChange}
-                      id="username"
-                      placeholder="username"
-                    />
-                    {/* <FormErrorMessage>err</FormErrorMessage> */}
-                  </FormControl>
-                )}
-              </Field>
+              <InputField
+                name="username"
+                placeholder="username"
+                label="Username"
+              />
+              <Box pt={5}>
+                <InputField
+                  name="password"
+                  placeholder="password"
+                  label="Password"
+                  type="password"
+                />
+              </Box>
+              <Box pt={5}>
+                <Button
+                  type="submit"
+                  colorScheme="teal"
+                  isLoading={isSubmitting}
+                >
+                  Register
+                </Button>
+              </Box>
             </Form>
           )}
         </Formik>
